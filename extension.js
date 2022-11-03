d824a644171cc88f295e9a51eb18d691675b80c4
POCHI-CUTE <takaono1789@gmail.com>
2022-11-04 00:05:33 +0900

    quote

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
+const vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,12 +16,12 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("sandarrow", function () {
+  let disposable = vscode.commands.registerCommand('sandarrow', function () {
     // The code you place here will be executed every time your command is executed
 
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
-      vscode.window.showWarningMessage("No active text editor found!");
+      vscode.window.showWarningMessage('No active text editor found!');
       return;
     }
     const selection = editor.selection;
@@ -32,12 +32,16 @@ function activate(context) {
     const text = editor.document.getText(cursorPosition);
 
     if (/<[^<>]*>/g.test(text)) {
-      vscode.commands.executeCommand("cursorMove", { to: "right" });
-      vscode.commands.executeCommand("cursorMove", { to: "right" });
+      vscode.commands.executeCommand('cursorMove', {
+        to: 'right',
+      });
+      vscode.commands.executeCommand('cursorMove', {
+        to: 'right',
+        value: 1,
+      });
     } else if (/\/[^<>]*>/g.test(text)) {
-      vscode.commands.executeCommand("cursorMove", { to: "left" });
-      vscode.commands.executeCommand("cursorMove", { to: "left" });
-      vscode.commands.executeCommand("cursorMove", { to: "left" });
+      vscode.commands.executeCommand('cursorMove', { to: 'left' });
+      vscode.commands.executeCommand('cursorMove', { to: 'left', value: 2 });
     }
     // Display a message box to the user
   });
